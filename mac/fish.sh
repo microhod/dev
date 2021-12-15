fe3360257923a0244c63fbab087215c70772532b
Sam Hodgkinson <55709069+microhod@users.noreply.github.com>
2021-12-15 12:04:04 +0000

    fix fish commands and use custom theme

--- mac/fish.sh
+++ mac/fish.sh
@@ -4,6 +4,6 @@
 echo "install fisher"
 curl -s https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish > /dev/null
 echo "install pure plugin"
-fish -c fisher install rafaelrinaldi/pure > /dev/null
-echo "install ayu theme"
-fish -c fisher install edouard-lopez/ayu-theme.fish > /dev/null
+fisher install rafaelrinaldi/pure > /dev/null
+echo "install theme"
+./theme.fish
---
+++ mac/theme.fish
@@ -0,0 +1,25 @@
+#! /usr/local/bin/fish
+
+set fish_color_autosuggestion   4c566a
+set fish_color_cancel           \x2dr
+set fish_color_command          81a1c1
+set fish_color_comment          434c5e
+set fish_color_cwd              green
+set fish_color_cwd_root         red
+set fish_color_end              88c0d0
+set fish_color_error            ebcb8b
+set fish_color_escape           00a6b2
+set fish_color_history_current  \x2d\x2dbold
+set fish_color_host             normal
+set fish_color_host_remote      yellow
+set fish_color_match            \x2d\x2dbackground\x3dbrblue
+set fish_color_normal           normal
+set fish_color_operator         00a6b2
+set fish_color_param            eceff4
+set fish_color_quote            a3be8c
+set fish_color_redirection      b48ead
+set fish_color_search_match     bryellow\x1e\x2d\x2dbackground\x3dbrblack
+set fish_color_selection        white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3dbrblack
+set fish_color_status           red
+set fish_color_user             brgreen
+set fish_color_valid_path       \x2d\x2dunderline
