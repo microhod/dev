1a6394c5f070803015405ce71dee26406c8abb2d
Sam Hodgkinson <55709069+microhod@users.noreply.github.com>
2021-12-16 17:31:55 +0000

    add gpg setup

--- mac/setup.sh
+++ mac/setup.sh
@@ -70,3 +70,44 @@ then
 else
     echo "using existing global git email: ${email}"
 fi
+
+echo -e "\n--- gpg ---\n"
+
+gpg_id=$(git config --global user.signingkey)
+if [ -z "${gpg_id}" ]
+then
+    read -p "Do you want to set up GPG commit signing? (y/n) " confirm
+    if [ "${confirm}" == "y" ]
+    then
+        read -p "Pass the ID of the GPG key you want to use (default: generate new key): " gpg_id
+        if [ -z "${gpg_id}" ]
+        then
+            if [ ! -d "/Applications/GPG Keychain.app" ]
+            then
+                echo "error: GPG Keychain app not found"
+                exit 1
+            fi
+            open "/Applications/GPG Keychain.app"
+            read -p "Pass the ID of the newly created key you want to use: " gpg_id
+        fi
+        if [ -z "${gpg_id}" ]
+        then
+            echo "error: GPG ID empty"
+            exit 1
+        fi
+
+        echo "setting global commit signing for GPG key ${gpg_id}"
+        git config --global user.signingkey "${gpg_id}"
+        echo "enabling commit signing by default"
+        git config --global commit.gpgsign true
+        echo "setting GPG_TTY variable"
+        export GPG_TTY=$(tty)
+    fi
+else
+    echo "commit signing already set up with GPG key: ${gpg_id}"
+fi
+
+if [ ! -z "${gpg_id}" ]
+then
+    echo "run 'gpg --export --armor ${gpg_id}' to export the key for use in github etc"
+fi
